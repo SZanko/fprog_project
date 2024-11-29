389e1af15e518d2943ca423c78c0a6cc6f92d417
SZanko <szanko_dev@proton.me>
2024-11-29 17:26:37 +0100

    Fix find-value-node function
    Signed-off-by: SZanko <szanko_dev@proton.me>

--- src/fprog_project/core.clj
+++ src/fprog_project/core.clj
@@ -11,7 +11,21 @@
 
 (def empty-tree nil)
 
+(defn is-red-node
+  "Checks if the node is red"
+  [^TreeNode node]
+  (->> (:color node)
+       (= red)
+       (and node)
+       ))
 
+(defn is-black-node
+  "Checks if the node is black"
+  [^TreeNode node]
+  (->> (:color node)
+       (= black)
+       (and node)
+       ))
 
 (defn balance
   "Ensures the given subtree stays balanced by rearranging black nodes
@@ -32,29 +46,31 @@
 
 (defn insert-val
   "Inserts x in tree.
-  Returns a node with x and no children if tree is nil.
+  Returns a node with node and no children if tree is nil.
 
   Returned tree is balanced. See also `balance`"
-  [tree x]
+  [^TreeNode tree ^TreeNode node]
   (let [ins (fn ins [tree]
               (match tree
-                     nil [:red nil x nil]
-                     [color a y b] (cond
-                                     (< x y) (balance [color (ins a) y b])
-                                     (> x y) (balance [color a y (ins b)])
-                                     :else tree)))
+                     [nil] [:red nil node nil]
+                     [color a value b] (cond
+                                         (< node value) (balance [color (ins a) value b])
+                                         (> node value) (balance [color a value (ins b)])
+                                         :else tree)))
         [_ a y b] (ins tree)]
     [:black a y b]))
 
-(defn find-val
-  "Finds value x in tree"
+(defn find-val-node
+  "Finds node with value x in tree"
   [tree x]
-  (match tree
-         nil nil
-         [_ a y b] (cond
-                     (< x y) (recur a x)
-                     (> x y) (recur b x)
-                     :else x)))
+  (match [tree]
+         [nil] nil ;; Match when tree is nil
+         [TreeNode]
+         (cond
+           (< (compare x (:value tree)) 0) (recur (:left tree) x)  ;; Search left subtree
+           (> (compare x (:value tree)) 0) (recur (:right tree) x) ;; Search right subtree
+           :else tree)))                         ;; Found the value
+
 
 
 (defn read-words
@@ -66,28 +82,22 @@
     (#(str/split % #"\s+"))
     ))
 
-(defn word-ascii-value [word]
-  (reduce + (map int word)))
-
-
-
-;((defn word-comparer
-;   "compares two words by ascii value and if both have the same sum, it will be alphabetically sorted,
-;    returns a set of the sorted words"
-;   [firstWord, secondWord]
-;   ((let [firstAscii (word-ascii-value firstWord)
-;          secondAscii (word-ascii-value secondWord)]
-;      )
-;    )
-;  ))
+(def example-tree
+  (->TreeNode
+   :black
+        (->TreeNode :red nil "Funktionale Programmierung" nil)                              ;; Left subtree
+        "Data Science"
+        (->TreeNode :red nil "Informatik" nil)))                          ;; Right subtree
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (->>
     ;(read-words "resources/war_and_peace_short.txt")
-    (read-words "resources/test_file_punctuation_numbers.txt")
-    (last)
+    ;(read-words "resources/test_file_punctuation_numbers.txt")
+    ;(last)
+    (find-val-node example-tree "Informatik")
+    ;(find-val-node empty-tree "Tmp")
     (println)
     )
   )
--- test/fprog_project/core_test.clj
+++ test/fprog_project/core_test.clj
@@ -5,3 +5,9 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 1 1))))
+
+(deftest find-val-test
+  (testing "find-val function"
+    (is (= (find-val-node empty-tree "Tmp") nil)))
+    (is (= (find-val-node example-tree "Informatik") (->TreeNode :red nil "Informatik" nil) ))
+  )
