a0727d92d8f7e39a2f9bd8d5630a9b0816005fde
sebwashere <sebi@toepfer.at>
2024-12-17 16:43:59 +0100

    new tests second iteration

--- test/fprog_project/core_test.clj
+++ test/fprog_project/core_test.clj
@@ -2,23 +2,6 @@
   (:require [clojure.test :refer :all]
             [fprog-project.core :refer :all]))
 
-(deftest test-is-red-node
-  (testing "is-red-node function"
-    (let [red-node (->TreeNode :red nil "test" nil)
-          black-node (->TreeNode :black nil "test" nil)
-          nil-node nil]
-      (is (is-red-node red-node) "Red node should return true")
-      (is (not (is-red-node black-node)) "Black node should return false")
-      (is (not (is-red-node nil-node)) "Nil node should return false"))))
-
-(deftest test-is-black-node
-  (testing "is-black-node function"
-    (let [red-node (->TreeNode :red nil "test" nil)
-          black-node (->TreeNode :black nil "test" nil)
-          nil-node nil]
-      (is (is-black-node black-node) "Black node should return true")
-      (is (not (is-black-node red-node)) "Red node should return false")
-      (is (not (is-black-node nil-node)) "Nil node should return false"))))
 
 (deftest test-branch?
   (testing "branch? function"
@@ -32,6 +15,21 @@
       (is (branch? branch-node) "Node with children should return true")
       (is (not (branch? nil-node)) "Nil node should return false"))))
 
+
+(deftest negative-test-branch?
+  (testing "branch? should correctly identify nodes with children"
+    (let [leaf-node (->TreeNode :black nil "leaf" nil)
+          left-branch-node (->TreeNode :black (->TreeNode :red nil "left" nil) "node" nil)
+          right-branch-node (->TreeNode :black nil "node" (->TreeNode :red nil "right" nil))
+          full-branch-node (->TreeNode :black
+                                       (->TreeNode :red nil "left" nil)
+                                       "node"
+                                       (->TreeNode :red nil "right" nil))]
+      (is (not (branch? leaf-node)) "Leaf node without children should not be a branch")
+      (is (branch? left-branch-node) "Node with left child should be a branch")
+      (is (branch? right-branch-node) "Node with right child should be a branch")
+      (is (branch? full-branch-node) "Node with both children should be a branch"))))
+
 (deftest test-children
   (testing "children function"
     (let [leaf-node (->TreeNode :black nil "leaf" nil)
@@ -42,6 +40,20 @@
       (is (empty? (children leaf-node)) "Leaf node should return empty list")
       (is (= 2 (count (children branch-node))) "Branch node should return two children"))))
 
+(deftest negative-test-children
+  (testing "children should return correct number of children"
+    (let [leaf-node (->TreeNode :black nil "leaf" nil)
+          left-child-node (->TreeNode :black (->TreeNode :red nil "left" nil) "node" nil)
+          right-child-node (->TreeNode :black nil "node" (->TreeNode :red nil "right" nil))
+          full-branch-node (->TreeNode :black
+                                       (->TreeNode :red nil "left" nil)
+                                       "node"
+                                       (->TreeNode :red nil "right" nil))]
+      (is (empty? (children leaf-node)) "Leaf node should have no children")
+      (is (= 1 (count (children left-child-node))) "Node with left child should have one child")
+      (is (= 1 (count (children right-child-node))) "Node with right child should have one child")
+      (is (= 2 (count (children full-branch-node))) "Node with both children should have two children"))))
+
 (deftest test-get-tree-content
   (testing "get-tree-content function"
     (let [tree (->TreeNode
@@ -51,12 +63,33 @@
                  (->TreeNode :red nil "F" nil))]
       (is (= ["B" "D" "F"] (get-tree-content tree)) "Should return in-order traversal"))))
 
+(deftest negative-test-get-tree-content
+  (testing "get-tree-content should handle various tree structures"
+    (let [empty-tree nil
+          single-node (->TreeNode :black nil "single" nil)
+          unbalanced-left-tree (->TreeNode
+                                 :black
+                                 (->TreeNode :red nil "left" nil)
+                                 "root"
+                                 nil)
+          unbalanced-right-tree (->TreeNode
+                                  :black
+                                  nil
+                                  "root"
+                                  (->TreeNode :red nil "right" nil))]
+      (is (empty? (get-tree-content empty-tree)) "Empty tree should return empty sequence")
+      (is (= ["single"] (get-tree-content single-node)) "Single node tree should return its value")
+      (is (= ["left" "root"] (get-tree-content unbalanced-left-tree)) "Left unbalanced tree should return values in-order")
+      (is (= ["root" "right"] (get-tree-content unbalanced-right-tree)) "Right unbalanced tree should return values in-order"))))
+
+
 (deftest test-invert-colors
   (testing "invert-colors function"
     (let [black-node (->TreeNode :black nil "test" nil)
           red-node (->TreeNode :red nil "test" nil)]
       (is (= :red (:color (invert-colors black-node))) "Black node should become red")
       (is (= :black (:color (invert-colors red-node))) "Red node should become black"))))
+
 
 (deftest test-rotate-right
   (testing "rotate-right function"
@@ -98,18 +131,6 @@
       (is (= :black (:color tree-with-first)) "First inserted node should be black")
       (is (= "first" (:value tree-with-first)) "First node value should be correct")
       (is (some? tree-with-two-nodes) "Should be able to insert second node"))))
-
-(deftest test-find-val-node
-  (testing "find-val-node function"
-    (let [tree (->TreeNode
-                 :black
-                 (->TreeNode :red nil "B" nil)
-                 "D"
-                 (->TreeNode :red nil "F" nil))]
-      (is (= "B" (:value (find-val-node tree "B"))) "Should find left node")
-      (is (= "D" (:value (find-val-node tree "D"))) "Should find root node")
-      (is (= "F" (:value (find-val-node tree "F"))) "Should find right node")
-      (is (nil? (find-val-node tree "X")) "Should return nil for non-existent value"))))
 
 (deftest test-read-words
   (testing "read-words function"
